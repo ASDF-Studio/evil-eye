b08e2c9bcb71427dfd8c8a17c99632d11d7741b2
TIRony <rony120114@gmail.com>
2024-04-04 11:04:22 +0600

    testing

--- src/layout/index.js
+++ src/layout/index.js
@@ -12,7 +12,7 @@ const MainLayout = ({ children }) => {
       <Header />
       <div className="h-screen overflow-hidden">
         <div className="block lsm:hidden">
-          <BG420 />
+          {/* <BG420 /> */}
         </div>
         <div className="hidden lsm:block">
           <BG960 />
@@ -23,7 +23,7 @@ const MainLayout = ({ children }) => {
         <div className="hidden 5xl:block">
           <BG2880 />
         </div>
-        {/* <BGvideo /> */}
+        <BGvideo />
 
         <div className="z-10 relative mt-16 overflow-y-auto h-full sm:mt-0 sm:h-full  sm:overflow-hidden">{children}</div>
       </div>
