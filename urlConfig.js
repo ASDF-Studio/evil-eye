deae28f0a44e314071467298c422c5cd06a8f0db
TIRony <rony120114@gmail.com>
2024-04-28 15:09:28 +0600

    server update

--- urlConfig.js
+++ urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:5000/";
-// const baseUrl = "https://backend.evileye.com/";
+// const baseUrl = "http://localhost:5000/";
+const baseUrl = "https://evileyeback.asdfstudio.com/";
 
 export const API = `${baseUrl}api/`;
