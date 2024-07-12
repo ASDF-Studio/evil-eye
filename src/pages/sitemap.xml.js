837bde45070e30df9a7f17d03be8d4fc704dbbe3
TIRony <rony120114@gmail.com>
2024-07-12 12:27:42 +0600

    added sitemap xml

---
+++ src/pages/sitemap.xml.js
@@ -0,0 +1,36 @@
+// pages/sitemap.xml.js
+
+const Sitemap = () => {
+    // Nothing to render for this route
+    return null;
+  };
+  
+  export const getServerSideProps = async ({ res }) => {
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+              http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+      <url>
+        <loc>https://evileyeremedy.com/</loc>
+        <lastmod>2024-07-12T05:48:14+00:00</lastmod>
+      </url>
+      <url>
+        <loc>https://evileyeremedy.com/privacy</loc>
+        <lastmod>2024-07-12T05:48:14+00:00</lastmod>
+      </url>
+    </urlset>`;
+  
+    res.setHeader('Content-Type', 'application/xml');
+    res.write(sitemap);
+    res.end();
+  
+    return {
+      props: {},
+    };
+  };
+  
+  export default Sitemap;
+  
