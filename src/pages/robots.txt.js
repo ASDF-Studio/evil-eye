cb0bd304d042a3eb8628193f95f473eb8a1bd96d
TIRony <rony120114@gmail.com>
2024-08-20 21:06:26 +0600

    added robots.txt

--- src/pages/robots.txt.js
+++ src/pages/robots.txt.js
@@ -1,9 +1,10 @@
-// pages/robots.txt.tsx
+// pages/robots.txt.js
+
 export async function getServerSideProps({ res }) {
     const robots = `
       User-agent: *
-      Disallow: /background/
-      Disallow: /audio/
+      Disallow: /background/*
+      Disallow: /audio/*
       Allow: /
   
       Sitemap: https://evileyeremedy.com/sitemap.xml
@@ -18,7 +19,8 @@ export async function getServerSideProps({ res }) {
     };
   }
   
-  export default function Robots() {
+  export default function RobotsTxt() {
+    // Default export to prevent Next.js errors
     return null;
   }
   
