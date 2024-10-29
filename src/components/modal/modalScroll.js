7c6fb749cefc9b225904c34957711c9415342dce
TIRony <rony120114@gmail.com>
2024-10-29 18:16:09 +0600

    fix the full yellow border on account

--- src/components/modal/modalScroll.js
+++ src/components/modal/modalScroll.js
@@ -3,10 +3,14 @@ import React from "react";
 const ModalScroll = ({ children, className = "" }) => {
   return (
     <div
-      className={`${className} h-auto max-h-[70svh] overflow-y-auto mb-3.5 scrollbar scrollbar-thumb-[#FFCE70] scrollbar-track-transparent scrollbar-corner-transparent sm:max-h-[75svh]`}
+      className={`${className} h-auto overflow-y-auto mb-3.5 scrollbar scrollbar-thumb-[#FFCE70] scrollbar-track-transparent scrollbar-corner-transparent`}
     >
       {children}
     </div>
   );
 };
 export default ModalScroll;
+
+{/* <div
+className={`${className} h-auto max-h-[70svh] overflow-y-auto mb-3.5 scrollbar scrollbar-thumb-[#FFCE70] scrollbar-track-transparent scrollbar-corner-transparent sm:max-h-[75svh]`}
+> */}
