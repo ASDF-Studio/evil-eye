6204534e55ee3f1be2f5a7d66e7634d579f0f71a
TIRony <rony120114@gmail.com>
2024-12-14 15:26:24 +0600

    update modal scroll frame

--- src/components/modal/modalScroll.js
+++ src/components/modal/modalScroll.js
@@ -3,7 +3,7 @@ import React from "react";
 const ModalScroll = ({ children, className = "" }) => {
   return (
 <div
-className={`${className} h-auto overflow-y-auto mb-3.5 scrollbar scrollbar-thumb-[#FFCE70] scrollbar-track-transparent scrollbar-corner-transparent`}
+className={`${className} h-auto max-h-[80svh] overflow-y-auto mb-3.5 scrollbar scrollbar-thumb-[#FFCE70] scrollbar-track-transparent scrollbar-corner-transparent sm:h-auto`}
 >
       {children}
     </div>
